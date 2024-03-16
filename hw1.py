ad9335a04781ab682a28d769b7cd2c8980a0e43e
gka6428 <162865825+gka6428@users.noreply.github.com>
2024-03-16 15:11:42 +0900

    과제제출[hw1.py]

--- hw1.py
+++ hw1.py
@@ -2,14 +2,14 @@
 #  사용자로부터 원의 반지름을 정수로 입력받아
 #  그 원의 넓이를 구하는 프로그램을 구하는 프로그램을 작성하자
 def get_radius(prompt) :
-    r = int(prompt)
+    r = int(input(prompt))
     return r
 
 def get_circle_area(radius) :
     circle_area = 3.14 * radius * radius
     return circle_area
 
-num = input('넓이를 구하고자 하는 원의 반지름은? ')
+num = '넓이를 구하고자 하는 원의 반지름은? '
 result = get_radius(num)
 s = get_circle_area(result)
 
