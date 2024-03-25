804b89bca7df34ed35e19b51a153538357f8360d
gka6428 <162865825+gka6428@users.noreply.github.com>
2024-03-25 12:59:34 +0900

    add task[hw2.py]

--- hw2.py
+++ hw2.py
@@ -12,15 +12,15 @@ nk (k는 자연수) == k원 동전의 개수
 # 사용자 함수 정의부
 def exchange(money) :
     n500 = money // 500 # 500으로 나누었을 때의 몫 == 500원 동전의 개수
-    balance1 = money % 500 # 500으로 나누었을 때의 나머지(금액) == 다음 단위동전에 할당할 잔액
+    balance = money % 500 # 500으로 나누었을 때의 나머지(금액) == 다음 단위동전에 할당할 잔액
 
-    n100 = balance1 // 100
-    balance2 = balance1 % 100
+    n100 = balance // 100
+    balance %= 100
     
-    n50 = balance2 // 50
-    balance3 = balance2 % 50
+    n50 = balance // 50
+    balance %= 50
     
-    n10 = balance3 // 10
+    n10 = balance // 10
 
     print('500원 동전의 개수:', n500)
     print('100원 동전의 개수:', n100)
